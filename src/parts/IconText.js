9d593e2cff2ad263da03fa4ef54f378b7e639370
suhada@pts.sc <rizkihalasan@pts.sc>
2021-06-12 22:41:12 +0700

    add hero and most picked pages

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -3,8 +3,10 @@ import Button from 'elements/button'
 
 export default function IconText() {
     return (
+        <>
         <Button className="brand-text-icon" href="" type="link">
-            Stay<span className="text-gray-900">cation.</span>
+            Stay<span>cation.</span>
         </Button>
+        </>
     )
 }
