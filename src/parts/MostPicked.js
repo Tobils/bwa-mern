7c0e6c31f97d856929cba87ba71a094ef44f24ff
suhada@pts.sc <rizkihalasan@pts.sc>
2021-06-13 10:59:23 +0700

    add categories component

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -3,22 +3,21 @@ import Button from 'elements/button'
 
 export default function MostPicked(props) {
     return (
-        <section className="container" ref={props.refMostPicked} style={{ height: 2000 }}>
+        <section className="container" ref={props.refMostPicked}>
             <h4 className="mb-3"> Most Picked</h4>
             <div className="container-grid">
                 {
                     props.data.map((item, index) => {
                         return (
                             <div key={`most-picked-${index}`} 
-                            className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}>
+                                className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}>
                                 <div className="card card-featured">
                                     <div className="tag">
                                         ${item.price}
                                         <span className="font-weight-light">per {item.unit}</span>
                                     </div>
                                     <figure className="img-wrapper">
-                                        <img src={item.imageUrl} alt={item.name} className="img-cover">
-                                        </img>
+                                        <img src={item.imageUrl} alt={item.name} className="img-cover"/>
                                     </figure>
                                     <div className="meta-wrapper">
                                         <Button 
